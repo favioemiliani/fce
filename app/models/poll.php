efffc133f265d38a0607fa1db7e5a14ef7a88027
César Díaz <cesare.diaz@gmail.com>
2011-05-19 19:54:10 -0300

    - Modelo   * agrego una validacion de rango numerico para el dni     que va entre los 5 y 35 millones

--- app/models/poll.php
+++ app/models/poll.php
@@ -15,8 +15,12 @@ class Poll extends AppModel  {
       'doc_numberRule-2' => array(         
         'rule' => 'numeric',        
         'message' => 'Este campo tiene que ser numerico' 
-      ),   
-      'doc_numberRule-3'  => array(         
+      ),  
+     'doc_numberRule-3' => array(         
+        'rule' => array('range', 5000000,35000000),        
+        'message' => 'Este campo tiene que representarse entre 5000000 y 35000000' 
+      ),         
+      'doc_numberRule-4'  => array(         
         'rule' => 'notEmpty',        
         'message' => 'Este campo no puede quedar vacio' 
       )
