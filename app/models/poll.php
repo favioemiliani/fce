7f20c6f791215f262c7d61f614305fe898077ff7
César Díaz <cesare.diaz@gmail.com>
2011-05-19 19:48:41 -0300

    - Model
      * agrego validacion a campo nro de documento para que sea Unico
      Numerico y no este en blanco

--- app/models/poll.php
+++ app/models/poll.php
@@ -3,12 +3,24 @@ class Poll extends AppModel  {
   var $name = 'Poll';
   var $belongsTo = array('Province' => array('className' => 'Province'));
   var $validate = array(
-    'apenom' => array(         
+    'apenom' => array(        
       'rule' => 'notEmpty',        
       'message' => 'Este campo no puede quedar vacio' ),
-    'doc_number'  => array(         
-      'rule' => 'notEmpty',        
-      'message' => 'Este campo no puede quedar vacio' )
+
+    'doc_number' => array(         
+      'doc_numberRule-1' => array(
+        'rule' => 'isUnique',        
+        'message' => 'Este campo tiene que ser único' 
+      ),
+      'doc_numberRule-2' => array(         
+        'rule' => 'numeric',        
+        'message' => 'Este campo tiene que ser numerico' 
+      ),   
+      'doc_numberRule-3'  => array(         
+        'rule' => 'notEmpty',        
+        'message' => 'Este campo no puede quedar vacio' 
+      )
+    )
     );
   } 
 ?>
